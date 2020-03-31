1469e61bf4444466ee6eadcbf6fb9dbafa86511d
NMan1 <nickmantini01@gmail.com>
2020-03-30 22:27:05 -0500

    First Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, abort, redirect
+from flask.json import jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def default():
+    return render_template("/html/home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
