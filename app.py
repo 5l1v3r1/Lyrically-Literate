94f52e8c92e592921e9a3a1bc2e73dc981393b4d
NMan1 <nickmantini01@gmail.com>
2020-03-31 16:09:47 -0500

    Added pages + secound news section
    Co-authored-by: Landinbik <landindweaver@gmail.com>

--- app.py
+++ app.py
@@ -8,8 +8,33 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def default():
+def home():
     return render_template("/html/home.html")
+
+
+@app.route("/featured")
+def featured():
+    return render_template("/html/home.html")
+
+
+@app.route("/lyrics")
+def lyrics():
+    return render_template("/html/lyrics.html")
+
+
+@app.route("/news")
+def news():
+    return render_template("/html/news.html")
+
+
+@app.route("/community")
+def community():
+    return render_template("/html/community.html")
+
+
+@app.route("/shop")
+def shop():
+    return render_template("/html/shop.html")
 
 
 if __name__ == "__main__":
